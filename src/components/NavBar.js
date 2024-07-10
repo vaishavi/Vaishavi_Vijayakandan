6b4a65573592e95e2661ca14f185ca43dc78d490
Vaishavi <vvaishavi99@gmail.com>
2024-07-10 14:06:11 -0400

    nabvar changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,6 +5,7 @@ import { ArrowDownCircleFill } from 'react-bootstrap-icons';
 
 export const NavBar = () => {
     const [scrolled, setScrolled] = useState(false);
+    const [expanded, setExpanded] = useState(false);
 
     useEffect(() => {
         const onScroll = () => {
@@ -21,30 +22,29 @@ export const NavBar = () => {
     }, []);
 
     return (
-        <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
-            <Container>
-                <Navbar.Brand href="#home"> 
-                    <img src={logo} className="brand-logo" alt="Logo" />
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav"> 
-                    <span className="navbar-toggler-icon"></span>
-                </Navbar.Toggle>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home" className="navbar-link">Home</Nav.Link>
-                        <Nav.Link href="#about" className="navbar-link">About</Nav.Link>
-                        <Nav.Link href="#skills" className="navbar-link">Skills</Nav.Link>
-                        <Nav.Link href="#projects" className="navbar-link">Project</Nav.Link>
-                        <Nav.Link href="/Vaishavi_Vijayakandan_SDE.pdf" className="navbar-link">Resume</Nav.Link>
-                    </Nav>
-                    <Nav>
-                        <Nav.Link href="#connect" className="navbar-link">
-                            Let's connect 
-                            <ArrowDownCircleFill size={25} style={{ marginLeft: '0.5rem' }} />
-                        </Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <Navbar expand="md" expanded={expanded} className={scrolled ? "scrolled" : ""}>
+    <Container>
+        <Navbar.Brand href="#home">
+            <img src={logo} className="brand-logo" alt="Logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(expanded ? false : true)} />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto" onSelect={() => setExpanded(false)}>
+                <Nav.Link href="#home" className="navbar-link">Home</Nav.Link>
+                <Nav.Link href="#about" className="navbar-link">About</Nav.Link>
+                <Nav.Link href="#skills" className="navbar-link">Skills</Nav.Link>
+                <Nav.Link href="#projects" className="navbar-link">Projects</Nav.Link>
+                <Nav.Link href="/Vaishavi_Vijayakandan_SDE.pdf" className="navbar-link">Resume</Nav.Link>
+            </Nav>
+            <Nav onSelect={() => setExpanded(false)}>
+                <Nav.Link href="#connect" className="navbar-link">
+                    Let's connect 
+                    <ArrowDownCircleFill size={25} style={{ marginLeft: '0.5rem' }} />
+                </Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+    </Container>
+</Navbar>
+
     );
 };
